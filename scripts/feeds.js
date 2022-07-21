795ccb4b4985a1b72423327850f6eb2581666b8a
shejiewu <32321072+shejiewu@users.noreply.github.com>
2022-07-21 14:28:19 +0800

    Add files via upload

---
+++ scripts/feeds.js
@@ -0,0 +1,34 @@
+/**
+ * 字段说明
+ * @name feed 名称
+ * @uri feed 仓库
+ * @branch feed 分支
+ */
+
+module.exports = [
+  {
+    name: 'open_app_filter',
+    uri: 'https://github.com/destan19/OpenAppFilter.git',
+    branch: 'oaf-5.1.0',
+  },
+  {
+    name: 'immortalwrt_luci',
+    uri: 'https://github.com/immortalwrt/luci.git',
+    branch: 'openwrt-21.02',
+  },
+  // {
+  //   name: 'lienol_packages',
+  //   uri: 'https://github.com/Lienol/openwrt-package.git',
+  //   branch: 'main',
+  // },
+  {
+    name: 'kenzok8_packages',
+    uri: 'https://github.com/kenzok8/openwrt-packages.git',
+    branch: 'master',
+  },
+  {
+    name: 'kenzok8_small',
+    uri: 'https://github.com/kenzok8/small.git',
+    branch: 'master',
+  },
+];
---
+++ scripts/packages.js
@@ -0,0 +1,73 @@
+
+/**
+ * 字段说明
+ * @name app 名称
+ * @desc app 描述
+ */
+module.exports = [
+  {
+    name: 'luci-theme-argon',
+    desc: 'argon主题',
+  },
+  {
+    name: 'luci-app-argon-config',
+    desc: 'argon主题设置',
+  },
+  {
+    name: 'luci-app-ramfree',
+    desc: '释放内存',
+  },
+  {
+    name: 'luci-app-wol',
+    desc: 'wol 网络唤醒',
+  },
+  {
+    name: 'luci-app-diskman',
+    desc: '磁盘管理工具',
+
+  {
+    name: 'luci-app-turboacc',
+    desc: 'Turbo ACC 网络加速(支持 Fast Path 或者 硬件 NAT)',
+  },
+  {
+    name: 'luci-app-cpufreq',
+    desc: 'CPU 性能优化调节',
+  },
+  {
+    name: 'luci-app-ssr-plus',
+    desc: 'ssr-plus 代理软件',
+  },
+  {
+    name: 'luci-app-ddns',
+    desc: '动态域名 DDNS',
+  },
+  },
+  {
+    name: 'luci-app-smartdns',
+    desc: 'smartdns DNS防污染',
+  },
+  {
+    name: 'luci-app-ddnsto',
+    desc: 'DDNSTO 内网穿透',
+  },
+  {
+    name: 'luci-app-wrtbwmon',
+    desc: '实时流量监测',
+  },
+  {
+    name: 'luci-app-serverchan',
+    desc: '微信/Telegram推送',
+  },
+  {
+    name: 'luci-app-transmission',
+    desc: 'BT下载工具',
+  },
+  {
+    name: 'luci-app-appfilter',
+    desc: '应用过滤'
+  },
+  // {
+  //   name: 'luci-app-samba4',
+  //   desc: 'samba4文件共享',
+  // },
+];
