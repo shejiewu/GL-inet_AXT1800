c1b2636f2d6aef6d781ae083784f9ce638d74cab
shejiewu <32321072+shejiewu@users.noreply.github.com>
2022-07-29 07:49:09 +0800

    Update packages.js

--- scripts/packages.js
+++ scripts/packages.js
@@ -6,16 +6,16 @@
  */
 module.exports = [
   {
+    "name": "luci-app-accesscontrol",
+    "desc": "访问时间控制"
+  },
+  {
     name: 'luci-theme-argon',
     desc: 'Argon主题',
   },
   {
     name: 'luci-app-argon-config',
     desc: 'Argon主题设置',
-  },
-  {
-    name: 'luci-app-diskman',
-    desc: '磁盘管理工具',
   },
   {
     "name": "luci-app-arpbind",
@@ -26,20 +26,44 @@ module.exports = [
     "desc": "支持计划重启"
   },
   {
-    "name": "luci-app-commands",
-    "desc": "Shell命令模块"
+    name: 'luci-app-ddns',
+    desc: '动态域名 DDNS',
+  },
+  {
+    name: 'luci-app-ddnsto',
+    desc: 'DDNSTO 内网穿透',
+  },
+  {
+    name: 'luci-app-diskman',
+    desc: '磁盘管理工具',
   },
   {
     "name": "luci-app-firewall",
     "desc": "添加防火墙"
   },
   {
-    "name": "luci-app-hd-idle",
-    "desc": "硬盘休眠"
+    "name": "luci-app-nlbwmon",
+    "desc": "网络带宽监视器"
   },
   {
-    "name": "luci-app-statistics",
-    "desc": "流量监控工具"
+    "name": "luci-app-ramfree",
+    "desc": "释放内存"
+  },
+  {
+    name: 'luci-app-samba4',
+    desc: 'Samba4文件共享',
+  },
+  {
+    name: 'luci-app-serverchan',
+    desc: '微信/Telegram推送',
+  },
+  {
+    name: 'luci-app-ssr-plus',
+    desc: 'SSR-Plus 代理',
+  },
+  {
+    name: 'luci-app-transmission',
+    desc: 'Transmission下载器',
   },
   {
     "name": "luci-app-ttyd",
@@ -54,43 +78,15 @@ module.exports = [
     "desc": "通用即插即用UPnP（端口自动转发）"
   },
   {
-    name: 'luci-app-samba4',
-    desc: 'Samba4文件共享',
-  },
-  {
-    name: 'luci-app-ddns',
-    desc: '动态域名 DDNS',
-  },
-  {
-    name: 'luci-app-smartdns',
-    desc: 'SmartDns_DNS防污染',
-  },
-  {
-    name: 'luci-compat',
-    desc: 'Compat依赖'
-  },
-  {
     name: 'luci-app-wol',
     desc: 'Wol 网络唤醒',
-  },
-  {
-    name: 'luci-app-ssr-plus',
-    desc: 'SSR-Plus 代理',
-  },
-  {
-    name: 'luci-app-ddnsto',
-    desc: 'DDNSTO 内网穿透',
   },
   {
     name: 'luci-app-wrtbwmon',
     desc: '实时流量监测',
   },
   {
-    name: 'luci-app-serverchan',
-    desc: '微信/Telegram推送',
-  },
-  {
-    name: 'luci-app-transmission',
-    desc: 'Transmission下载器',
+    name: 'luci-compat',
+    desc: 'Compat依赖'
   },
 ];
