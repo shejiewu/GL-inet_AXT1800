dfbf5eaa308f3be2756a00594f56ce4c450b2caf
shejiewu <32321072+shejiewu@users.noreply.github.com>
2022-07-28 21:05:46 +0800

    Add files via upload

--- scripts/packages.js
+++ scripts/packages.js
@@ -14,33 +14,37 @@ module.exports = [
     desc: 'Argon主题设置',
   },
   {
-    name: 'luci-app-ramfree',
-    desc: '释放内存',
+    name: 'luci-app-diskman',
+    desc: '磁盘管理工具',
+  },
+  {
+    name: 'luci-app-samba4',
+    desc: 'Samba4文件共享',
+  },
+  {
+    name: 'luci-app-ddns',
+    desc: '动态域名 DDNS',
+  },
+  {
+    name: 'ddns-scripts_cloudflare',
+    desc: 'DDNS_CloudFlare.com-v1',
+  },
+  {
+    name: 'ddns-scripts_cloudflare.com-v4',
+    desc: 'DDNS_CloudFlare.com-v4',
+  },
+  {
+    name: 'luci-compat',
+    desc: 'Compat依赖'
   },
   {
     name: 'luci-app-wol',
     desc: 'Wol 网络唤醒',
   },
   {
-    name: 'luci-app-diskman',
-    desc: '磁盘管理工具',
-  },
-  {
     name: 'luci-app-ssr-plus',
     desc: 'SSR-Plus 代理',
   },
-  // {
-  //   name: 'luci-app-vssr',
-  //   desc: 'Hello-World 代理',
-  // },
-  {
-    name: 'luci-app-ddns',
-    desc: '动态域名 DDNS',
-  },
-  // {
-  //   name: 'luci-app-smartdns',
-  //   desc: 'SmartDns DNS防污染',
-  // },
   {
     name: 'luci-app-ddnsto',
     desc: 'DDNSTO 内网穿透',
@@ -56,13 +60,5 @@ module.exports = [
   {
     name: 'luci-app-transmission',
     desc: 'Transmission下载器',
-  },
-  {
-    name: 'luci-app-samba4',
-    desc: 'Samba4文件共享',
-  },
-  {
-    name: 'luci-compat',
-    desc: 'Compat依赖'
   },
 ];
--- scripts/workflow.tpl
+++ scripts/workflow.tpl
@@ -84,7 +84,10 @@ jobs:
         git clone https://github.com/gl-inet/glinet4.x.git -b main /workdir/glinet
         ./scripts/feeds update -a
         ./scripts/feeds install -a
+        cp -r /workdir/etc files
         make defconfig
+        cd /workdir/gl-infra-builder/wlan-ap/openwrt/files/etc
+        echo "$(date +"%Y.%m.%d")" >./glversion
 
     - name: SSH connection to Actions
       uses: P3TERX/ssh2actions@v1.0.0
