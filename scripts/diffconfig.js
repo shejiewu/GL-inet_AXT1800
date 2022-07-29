1552d4c20134c9f103634d425f6070d0b7eb4218
shejiewu <32321072+shejiewu@users.noreply.github.com>
2022-07-30 00:28:36 +0800

    Update diffconfig.js

--- scripts/diffconfig.js
+++ scripts/diffconfig.js
@@ -1,1 +1,14 @@
+/**
+ * 字段说明
+ * @name feed 名称
+ * @uri feed 仓库
+ * @branch feed 分支
+ */
 
+module.exports = [
+  {
+    name: 'immortalwrt_luci',
+    uri: 'https://github.com/immortalwrt/luci.git',
+    branch: 'openwrt-21.02',
+  },
+];
