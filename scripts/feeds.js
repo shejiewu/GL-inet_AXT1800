6daef383f0d984cca12b5caa7a7320efa3320aab
shejiewu <32321072+shejiewu@users.noreply.github.com>
2022-08-07 14:14:44 +0800

    Update feeds.js

--- scripts/feeds.js
+++ scripts/feeds.js
@@ -7,10 +7,6 @@
 
 module.exports = [
   {
-    name: 'default-settings',
-    path: '../feeds_dir/default-settings',
-  },
-  {
     name: 'immortalwrt_luci',
     uri: 'https://github.com/immortalwrt/luci.git',
     branch: 'openwrt-21.02',
