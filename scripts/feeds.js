1a52577e7e0784f590eaa6cb1e76a1f85c73132e
shejiewu <32321072+shejiewu@users.noreply.github.com>
2022-08-10 02:32:26 +0800

    Update feeds.js

--- scripts/feeds.js
+++ scripts/feeds.js
@@ -6,39 +6,9 @@
  */
 
 module.exports = [
-  // {
-  //   name: 'immortalwrt_luci',
-  //   uri: 'https://github.com/immortalwrt/luci.git',
-  //   branch: 'openwrt-21.02',
-  // },
   {
     name: 'NueXini_Packages',
     uri: 'https://github.com/NueXini/NueXini_Packages.git',
     branch: 'main',
   },
-  // {
-  //   name: 'liuran001_packages',
-  //   uri: 'https://github.com/liuran001/openwrt-packages.git',
-  //   branch: 'luci',
-  // },
-  // {
-  //   name: 'xiaorouji_passwall',
-  //   uri: 'https://github.com/xiaorouji/openwrt-passwall.git',
-  //   branch: 'packages',
-  // },
-  // {
-  //   name: 'lienol_packages',
-  //   uri: 'https://github.com/Lienol/openwrt-package.git',
-  //   branch: 'main',
-  // },
-  // {
-  //   name: 'kenzok8_packages',
-  //    uri: 'https://github.com/kenzok8/openwrt-packages.git',
-  //   branch: 'master',
-  // },
-  // {
-  //   name: 'kenzok8_small',
-  //   uri: 'https://github.com/kenzok8/small.git',
-  //   branch: 'master',
-  // },
 ];
