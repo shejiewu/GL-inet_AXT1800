45f1b569d8b5873f570c04dc1b574b68cf1d9237
shejiewu <32321072+shejiewu@users.noreply.github.com>
2022-08-12 00:24:20 +0800

    Update feeds.js

--- scripts/feeds.js
+++ scripts/feeds.js
@@ -7,11 +7,6 @@
 
 module.exports = [
   {
-    name: 'immortalwrt_luci',
-    uri: 'https://github.com/immortalwrt/luci.git',
-    branch: 'openwrt-21.02',
-  },
-  {
     name: 'NueXini_Packages',
     uri: 'https://github.com/NueXini/NueXini_Packages.git',
     branch: 'main',
