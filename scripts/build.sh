785e925f32e0fe3cb72e3c5888909071b2b1046d
shejiewu <32321072+shejiewu@users.noreply.github.com>
2022-08-11 23:15:53 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,19 +8,19 @@ read input
 
 case $input in
 1)
-		echo "编译AXT1800 - 4.X内核"
-		DEVICE0="wlan-ap"
-		DEVICE1="axt1800"
-		;;	
+    echo "编译AXT1800 - 4.X内核"
+    DEVICE0="wlan-ap"
+    DEVICE1="axt1800"
+    ;;	
 2)
-		echo "编译AXT1800 - 5.X内核"
-		DEVICE0="wlan-ap-5.4"
-		DEVICE1="axt1800-5-4"
-		;;
-		*) echo '请输入“1”或“2”（不带引号）选择，您的选择错误，脚本已退出，请重新启动脚本！！'
-		   echo '  '
-		   exit
-                ;;
+    echo "编译AXT1800 - 5.X内核"
+    DEVICE0="wlan-ap-5.4"
+    DEVICE1="axt1800-5-4"
+    ;;
+    *) echo '请输入“1”或“2”（不带引号）选择，您的选择错误，脚本已退出，请重新启动脚本！！'
+    echo '  '
+    exit
+    ;;
 esac
 
 git clone https://github.com/gl-inet/gl-infra-builder.git $PWD/gl-infra-builder
@@ -29,8 +29,8 @@ cp -r $PWD/*.yml $PWD/gl-infra-builder/profiles
 ## cd $PWD/gl-infra-builder && python3 setup.py -c configs/config-wlan-ap.yml
 cd $PWD/gl-infra-builder && python3 setup.py -c configs/config-$DEVICE0.yml
 
-## cd wlan-ap/openwrt && ./scripts/gen_config.py $PWD/profiles/glinet-axt1800 glinet_depends
-cd wlan-ap/openwrt && ./scripts/gen_config.py $PWD/profiles/glinet-$DEVICE1 glinet_depends
+## cd wlan-ap/openwrt && ./scripts/gen_config.py profiles/glinet-axt1800 glinet_depends
+cd wlan-ap/openwrt && ./scripts/gen_config.py profiles/glinet-$DEVICE1 glinet_depends
 
 git clone https://github.com/gl-inet/glinet4.x.git -b main $PWD/glinet
 
