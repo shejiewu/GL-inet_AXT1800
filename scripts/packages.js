dc82b0a92f4d5ce3acb588445111671b353c8062
shejiewu <32321072+shejiewu@users.noreply.github.com>
2022-08-19 00:53:16 +0800

    Update packages.js

--- scripts/packages.js
+++ scripts/packages.js
@@ -86,6 +86,14 @@ module.exports = [
     desc: '实时流量监测',
   },
   {
+    name: 'coremark',
+    desc: 'CPU_MARK跑分',
+  },
+  {
+    name: 'openssh-sftp-server',
+    desc: 'SSH_SFTP链接',
+  },
+  {
     name: 'luci-compat',
     desc: 'Compat依赖',
   },
